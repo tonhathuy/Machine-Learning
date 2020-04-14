f2fdb692685cde6dcd6077a3f85ac13b7ace39e7
tonhathuy <tonhathuy97@gmail.com>
2020-04-14 15:05:12 +0700

    change s/t

--- hello/opencv/smoothing_Image _Blurring_Images.py
+++ hello/opencv/smoothing_Image _Blurring_Images.py
@@ -8,6 +8,8 @@ img = cv.imread('Noise_salt_and_pepper.png')
 img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
 
 kernel = np.ones((5,5), np.float32)/25
+
+
 dst = cv.filter2D(img, -1, kernel)
 blur = cv.blur(img, (5, 5))
 gblur = cv.GaussianBlur(img, (5, 5), 0)
@@ -23,3 +25,4 @@ for i in range(6):
     plt.xticks([]), plt.yticks([])
 
 plt.show()
+
